39e5bc81811f98e87b11a5a6282623c8c191e2d8
babuy45 <ashabibi49@gmail.com>
2019-11-25 11:23:09 +0700

    vscode-docs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// // import logo from './logo.svg';
+// import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {nama:'Andi', usia:21};
+  }
+  render() {
+    setTimeout(() => {
+      this.setState({nama: 'Budi', usia: 25});
+    }, 3000)
+    return (
+      <div>
+        <h1>Halo nama saya {this.state.nama}</h1>
+        <h1>usia saya {this.state.usia} th</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
+
